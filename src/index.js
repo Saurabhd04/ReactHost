45952525ebe1619ede19a04a0a9739eea0d84f21
Saurabhd04 <dhoundiyals19@gmail.com>
2021-04-12 13:50:49 +0530

    Done 2

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ ReactDOM.render(
   //   <App />
   // </React.StrictMode>,
   <h1>Wingardium Leviosa!!</h1>,
+  <br></br>,
+  <h2>Wingardium Leviosa!!</h2>,
+  <br></br>,
+  <h3>Wingardium Leviosa!!</h3>,
   document.getElementById('root')
 );
 
